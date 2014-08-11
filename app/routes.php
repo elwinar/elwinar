e2fdb3f2e713fbb91a9d74e8cc08fa752f3a00de
Romain Baugue <romain.baugue@elwinar.com>
2014-08-11 15:48:44 +0200

    Fixed routes inclusion

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,7 @@
 <?php
 foreach(File::files(app_path().'/routes') as $file) {
 	Route::group(array(
-		'domain' => basename($file, '.php').'.'.domain()
+		'domain' => basename($file, '.php').'.'.Config::get('app.domain')
 	),function() use($file) {
 		include($file);
 	});
