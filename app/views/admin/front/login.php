1817ea3771c1d770153065b81cf2705b9d0d4e1b
Romain Baugue <romain.baugue@elwinar.com>
2014-09-20 13:06:31 +0200

    Google analytics

--- app/views/admin/front/login.php
+++ app/views/admin/front/login.php
@@ -1,5 +1,5 @@
-            <form method="POST">
-                <input class="form-control" type="text" name="username" placeholder="Identifiant"><br>
-                <input class="form-control" type="password" name="password" placeholder="Mot de passe"><br>
-                <input class="form-control" type="submit" value="Connexion">
-            </form>
+			<form method="POST">
+				<input class="form-control" type="text" name="username" placeholder="Identifiant"><br>
+				<input class="form-control" type="password" name="password" placeholder="Mot de passe"><br>
+				<input class="form-control" type="submit" value="Connexion">
+			</form>
