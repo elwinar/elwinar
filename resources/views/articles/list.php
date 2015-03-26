dbae4b4a7c3d2782277956e280f0a41bb3f0ef0f
Romain Baugue <rb@capitaldata.fr>
2015-03-26 11:27:14 +0100

    Fix the publish/unpublish links

--- resources/views/articles/list.php
+++ resources/views/articles/list.php
@@ -10,9 +10,9 @@
 <?php 	if(Auth::check()) { ?>
 					<nav>
 <?php		if($article->is_published) { ?>
-						<a href="/article/<?= $article->slug ?>/publish" title="publish"><span class="fa fa-eye-slash"></span></a>
+						<a href="/article/<?= $article->slug ?>/unpublish" title="unpublish"><span class="fa fa-eye-slash"></span></a>
 <?php		} else { ?>
-						<a href="/article/<?= $article->slug ?>/unpublish" title="unpublish"><span class="fa fa-eye"></span></a>
+						<a href="/article/<?= $article->slug ?>/publish" title="publish"><span class="fa fa-eye"></span></a>
 <?php		} ?>
 						<a href="/article/<?= $article->slug ?>/edit" title="edit"><span class="fa fa-pencil"></span></a>
 						<a href="/article/<?= $article->slug ?>/delete" title="delete"><span class="fa fa-trash-o"></span></a>
