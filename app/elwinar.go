3cb58751d9e3e28c618140166369b2cc09a1c409
Romain Baugue <romain.baugue@elwinar.com>
2015-04-03 22:34:44 +0200

    Remove default values from command line arguments

--- app/elwinar.go
+++ app/elwinar.go
@@ -17,31 +17,26 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.IntFlag{
 			Name:   "port",
-			Value:  80,
 			Usage:  "listening port",
 			EnvVar: "ELWINAR_PORT",
 		},
 		cli.StringFlag{
 			Name:   "database",
-			Value:  "storage/database.sqlite",
 			Usage:  "sqlite database file",
 			EnvVar: "ELWINAR_DATABASE",
 		},
 		cli.StringFlag{
 			Name:   "secret",
-			Value:  "",
 			Usage:  "encryption secret",
 			EnvVar: "ELWINAR_SECRET",
 		},
 		cli.StringFlag{
 			Name:   "password",
-			Value:  "",
 			Usage:  "administrative password",
 			EnvVar: "ELWINAR_PASSWORD",
 		},
 		cli.StringFlag{
 			Name:   "views",
-			Value:  "resources/views",
 			Usage:  "views repository",
 			EnvVar: "ELWINAR_VIEWS",
 		},
