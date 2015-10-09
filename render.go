e26e8d46b9837bc9d17bc31ed75d3a5146d55aad
Romain Baugue <romain.baugue@elwinar.com>
2015-10-09 16:47:02 +0200

    Remove the errors and inputs from the render function

--- render.go
+++ render.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"net/http"
-	"net/url"
+import "net/http"
 
-	sessions "github.com/goincremental/negroni-sessions"
-)
-
-func render(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
-	var rawErrs = sessions.GetSession(r).Flashes("_errors")
-	var errs []string
-	if len(rawErrs) != 0 {
-		errs = rawErrs[0].([]string)
-	}
-
-	var rawInputs = sessions.GetSession(r).Flashes("_inputs")
-	var inputs = new(url.Values)
-	if len(rawInputs) != 0 {
-		inputs = rawInputs[0].(*url.Values)
-	}
-
+func render(w http.ResponseWriter, name string, data interface{}) {
 	t, found := templates[name]
 	if !found {
 		panic("unknown template " + name)
