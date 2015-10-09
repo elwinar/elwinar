8332ec5e9c6e8d38a16ff6c7b54e571d1fd1ba5b
Romain Baugue <romain.baugue@elwinar.com>
2015-10-09 16:34:36 +0200

    Fix the calls to time.Now()

--- index.go
+++ index.go
@@ -12,6 +12,6 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	render(w, r, "index", map[string]interface{}{
 		"Title": "Passionate developer",
-		"Age":   time.Now.Year() - 1990, // I'm born 1990-11-05, and don't care about month precision for this….
+		"Age":   time.Now().UTC().Year() - 1990, // I'm born 1990-11-05, and don't care about month precision for this….
 	})
 }
