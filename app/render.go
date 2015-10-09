433078c61b60b1aca24278d50838b92f45d055f4
Romain Baugue <romain.baugue@elwinar.com>
2015-10-09 14:53:40 +0200

    Start rewriting the render function

--- app/render.go
+++ app/render.go
@@ -1,56 +1,11 @@
 package main
 
 import (
-	"encoding/gob"
-	"html/template"
 	"net/http"
 	"net/url"
-	"os"
-	"path/filepath"
-	"strconv"
-	"time"
 
 	sessions "github.com/goincremental/negroni-sessions"
-	"github.com/russross/blackfriday"
 )
-
-func init() {
-	gob.Register(&url.Values{})
-}
-
-func AgeHelper(birthdate string) (string, error) {
-	var birth, err = time.Parse("2006-01-02", birthdate)
-	if err != nil {
-		return "", err
-	}
-
-	var now = time.Now().UTC()
-	if now.YearDay() < birth.YearDay() {
-		return strconv.Itoa(now.Year() - birth.Year() - 1), nil
-	}
-
-	return strconv.Itoa(now.Year() - birth.Year()), nil
-}
-
-func BaseHelper() string {
-	return configuration.Base
-}
-
-func DateFormatHelper(format string, date time.Time) string {
-	return date.Format(format)
-}
-
-func EnvHelper(name string) string {
-	return os.Getenv(name)
-}
-
-func MarkdownHelper(text string) template.HTML {
-	return template.HTML(blackfriday.MarkdownCommon([]byte(text)))
-}
-
-func GoogleAnalyticsIDHelper() string {
-	return configuration.GID
-}
 
 func render(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
 	var rawErrs = sessions.GetSession(r).Flashes("_errors")
@@ -65,28 +20,9 @@ func render(w http.ResponseWriter, r *http.Request, name string, data interface{
 		inputs = rawInputs[0].(*url.Values)
 	}
 
-	t, err := template.New(name).Funcs(template.FuncMap{
-		"Age":        AgeHelper,
-		"Base":       BaseHelper,
-		"DateFormat": DateFormatHelper,
-		"Errors": func() []string {
-			return errs
-		},
-		"Env":               EnvHelper,
-		"GoogleAnalyticsID": GoogleAnalyticsIDHelper,
-		"Input": func() *url.Values {
-			return inputs
-		},
-		"Logged": func() interface{} {
-			return sessions.GetSession(r).Get("logged")
-		},
-		"Markdown": MarkdownHelper,
-		"Session": func(name string) interface{} {
-			return sessions.GetSession(r).Get(name)
-		},
-	}).ParseFiles(filepath.Join(configuration.Views, "app.html"), filepath.Join(configuration.Views, name+".html"))
-	if err != nil {
-		panic("unable to parse template " + name + ":" + err.Error())
+	t, found := templates[name]
+	if !found {
+		panic("unknown template " + name)
 	}
 
 	t.ExecuteTemplate(w, "app", data)
---
+++ app/templates.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"template/html"
+	"text/template"
+)
+
+var templates map[string]*html.Template
+
+func init() {
+	templates = make(map[string]*html.Template)
+
+	raw, err := Asset("../views/app.html")
+	if err != nil {
+		panic(err)
+	}
+
+	layout, err := template.New("layout").Parse(string(raw))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, template := range []string{
+		"article_view",
+		"article_form",
+		"article_list",
+		"article_edit",
+		"fortune",
+		"index",
+		"login",
+	} {
+		t, _ := layout.Clone()
+
+		raw, err := Asset("../views/" + template + ".html")
+		if err != nil {
+			panic(err)
+		}
+
+		t, err = t.Parse(string(raw))
+		if err != nil {
+			panic(err)
+		}
+
+		templates[template] = t
+	}
+}
