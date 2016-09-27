fdf0737c51caab0d6d89dc92907a30d30c9f7708
Romain Baugue <romain.baugue@elwinar.com>
2016-09-27 23:39:37 +0200

    Switch back to GoogleAnalytics

--- src/elwinar/configuration.go
+++ src/elwinar/configuration.go
@@ -3,10 +3,11 @@ package main
 // Configuration holds the configuration for the website: which port to listen on,
 // which database to use, etc.
 type Configuration struct {
-	Database string
-	Debug    bool
-	Password string
-	Port     int
-	Public   string
-	Secret   string
+	Database       string
+	Debug          bool
+	Password       string
+	Port           int
+	Public         string
+	Secret         string
+	AnalyticsToken string
 }
